a8caa116080fd81c4b435fb996cc438dd4d7670f
MGHN93 <mgilanghn@gmail.com>
2026-05-11 16:31:28 +0700

    set up exam 2

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php 
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'perpustakaan';
+
+$db = new mysqli($hostname, $username, $password, $database);
+
+echo "connected to database successfully";
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+require __DIR__ . "/config/database.php";
+require __DIR__ . "/includes/header.php";
+?>
+
+<h1>test</h1>
