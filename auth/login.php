df7da9309db178b5bd1fc54b02d56a0e7b8c5009
MGHN93 <mgilanghn@gmail.com>
2026-06-03 08:24:47 +0700

    deployment changes

--- auth/login.php
+++ auth/login.php
@@ -49,11 +49,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <form method="post">
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username">
+                <input type="text" class="form-control" id="username" name="username" placeholder="'admin'">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="'admin123'">
             </div>
 
             <button type="submit" class="login-button">LOGIN</button>
